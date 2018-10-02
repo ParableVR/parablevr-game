b88c699268e94ef247a924d906b35e024c019d7c
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-10-03 11:13:44 +1300

    only transform on exact ingredients in mixer

--- Assets/Scripts/Mixer.cs
+++ Assets/Scripts/Mixer.cs
@@ -25,39 +25,47 @@ namespace parable
 
             List<GameObject> gameObjects = cols
                 .Select(x => x.gameObject)
-                .Where(x =>
-                    x.gameObject.GetComponent<CloudComponent>().cId == "5bb31f90023eb3359042462e" || // aluminum chips
-                    x.gameObject.GetComponent<CloudComponent>().cId == "5bb31f99023eb3359042462f") // iron oxide powder
+                .Where(x => x.gameObject.GetComponent<CloudComponent>().cId != null) // filter only gameobjects with cloudcomponents
                 .ToList();
 
-            if (gameObjects.Count == 2)
+            if (gameObjects.Count == 2) // should only be two items
             {
-                gameObjects.ForEach(obj =>
+                if (gameObjects
+                    .Where(x =>
+                        x.gameObject.GetComponent<CloudComponent>().cId == "5bb31f90023eb3359042462e" || // aluminum chips
+                        x.gameObject.GetComponent<CloudComponent>().cId == "5bb31f99023eb3359042462f") // iron oxide powder
+                    .ToList().Count == 2)
                 {
-                    Destroy(obj);
-                });
+                    // after selecting specific ids, should still be a count of two
+                    gameObjects.ForEach(obj =>
+                    {
+                        // remove the objects to replace them with the thermite
+                        Destroy(obj);
+                    });
 
-                GameObject gameObject = (GameObject)Instantiate(
-                    Resources.Load("Bottles/Mesh/Bottle 01", typeof(GameObject)),
-                    centre,
-                    new Quaternion(0, 0, 0, 1),
-                    GameObject.Find("/SceneContent/CloudObjects").transform);
+                    // load in the thermite
+                    GameObject gameObject = (GameObject)Instantiate(
+                        Resources.Load("Bottles/Mesh/Bottle 01", typeof(GameObject)),
+                        centre,
+                        new Quaternion(0, 0, 0, 1),
+                        GameObject.Find("/SceneContent/CloudObjects").transform);
 
-                gameObject.layer = 8; // layer 8 = cloudobjects layer
-                gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                    gameObject.layer = 8; // layer 8 = cloudobjects layer
+                    gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
 
-                gameObject.name = "Thermite";
-                gameObject.AddComponent<ObjectPopupName>(); // show name on focus
+                    gameObject.name = "Thermite";
+                    gameObject.AddComponent<ObjectPopupName>(); // show name on focus
 
-                // add various props from the cloudobject that aren't already present
-                CloudComponent cloudComponent = gameObject.AddComponent<CloudComponent>();
-                cloudComponent.cId = null; // these values will eventually be from the db
-                cloudComponent.cSignificant = true;
+                    // add various props from the cloudobject that aren't already present
+                    CloudComponent cloudComponent = gameObject.AddComponent<CloudComponent>();
+                    cloudComponent.cId = null; // these values will eventually be from the db
+                    cloudComponent.cSignificant = true;
 
-                // components required for picking up the object
-                gameObject.AddComponent<HoloToolkit.Unity.InputModule.HandDraggable>();
-                gameObject.AddComponent<Rigidbody>();
-                gameObject.AddComponent<BoxCollider>();
+                    // components required for picking up the object
+                    gameObject.AddComponent<HoloToolkit.Unity.InputModule.HandDraggable>();
+                    gameObject.AddComponent<Rigidbody>();
+                    gameObject.AddComponent<BoxCollider>();
+                }
             }
         }
     }
