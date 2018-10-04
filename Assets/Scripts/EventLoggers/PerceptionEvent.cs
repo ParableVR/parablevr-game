33ec85631c24d56d51615295f6af44a011fe4784
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-10-04 18:58:31 +1300

    log perception events to the cloud

--- Assets/Scripts/EventLoggers/PerceptionEvent.cs
+++ Assets/Scripts/EventLoggers/PerceptionEvent.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
+using Newtonsoft.Json;
+using parable.objects;
 
 namespace parable.eventloggers
 {
@@ -13,7 +15,7 @@ namespace parable.eventloggers
         {
             timer = new Stopwatch();
         }
-        
+
         void Update()
         {
 
@@ -29,7 +31,29 @@ namespace parable.eventloggers
             timer.Stop();
 
             // check elapsed, if it's long enough, log it
-            UnityEngine.Debug.Log(timer.ElapsedMilliseconds);
+            if (timer.ElapsedMilliseconds > 1000 * 1) // 1 sec
+            {
+                Dictionary<string, string> postHeaders = new Dictionary<string, string>();
+                postHeaders.Add("Content-Type", "application/json");
+
+                byte[] postData = System.Text.Encoding.UTF8
+                    .GetBytes(JsonConvert.SerializeObject(new
+                    {
+                        type = "perception",
+                        user = "",
+                        contact_duration = timer.ElapsedMilliseconds,
+                        result = false,
+                        object_coordinator = "",
+                        objects_involved = new string[]
+                        {
+                            gameObject.GetComponent<CloudComponent>().cId
+                        }
+                    }));
+
+                WWW www = new WWW(
+                    "https://parablevr-game-api.azurewebsites.net/api/events/do/create/5bb4259bcc7ed83ee3cfbd89?code=WQEsFmDQHSx42xbo46EJetmg4cSeZevInEYdJm2K4axhWlGl8Y19CQ==",
+                    postData, postHeaders);
+            }
 
             timer.Reset();
         }
