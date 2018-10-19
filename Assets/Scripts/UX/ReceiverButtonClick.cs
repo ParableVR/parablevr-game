fef899ba62504afcfce811ee416d54e51ef299e1
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-10-19 14:10:28 +1300

    welcome and scenario dialog

---
+++ Assets/Scripts/UX/ReceiverButtonClick.cs
@@ -0,0 +1,21 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.Receivers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiverButtonClick : InteractionReceiver {
+    protected override void InputDown(GameObject gameObject, InputEventData eventData)
+    {
+        Debug.Log(gameObject.name + " : InputDown");
+
+        switch (gameObject.name)
+        {
+            case "DialogWelcomeConnectButton":
+                // welcome button pressed
+                GameObject.Find("/SceneContent/SessionStart/DialogWelcome").SetActive(false); // disable this dialog
+                GameObject.Find("/SceneContent/SessionStart/DialogSelectScenario").SetActive(true); // show next dialog
+                break;
+        }
+    }
+}
